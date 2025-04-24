97772c5bd26f93a1ed0e0a0d2d09e301b321b104
DeividAlbeche <deividtrindade2005@gmail.com>
2025-04-24 12:03:10 -0300

    criando registro na classe principal

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,7 +1,11 @@
 package principal;
 
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import entidades.Cliente;
 import entidades.Veiculo;
@@ -43,6 +47,39 @@ public class Principal {
 		System.out.println();
 		
 		
+		// registro
+		
+		 Scanner leia = new Scanner(System.in);
+		
+		double valor_hora = 0;
+		
+		System.out.println("tipo do veiculo: 1-Carro 2-Moto");
+		int tipo_veiculo = leia.nextInt();
+		
+		if(tipo_veiculo == 1) {
+			valor_hora = 5;
+		}else if(tipo_veiculo == 2) {
+			valor_hora = 3;
+		}else {
+			System.out.println("Não existe essa opção");
+		}
+		
+		//registrando a entrada e a saida
+		Timestamp entrada = Timestamp.valueOf("2025-02-10 08:30:00");
+		Timestamp saida = Timestamp.valueOf("2025-02-10 10:31:00");
+		
+		//Convertendo para localDateTime
+		LocalDateTime entradaLDT = entrada.toLocalDateTime();
+		LocalDateTime saidaLDT = saida.toLocalDateTime();
+		
+		Duration duracao = Duration.between(entradaLDT, saidaLDT);
+
+		long horas = duracao.toHours();
+		long minutos = duracao.toMinutes() % 60;
+		
+		double valor_total = horas * valor_hora;
+		
+		System.out.print(valor_total);
 	}
 
 }
