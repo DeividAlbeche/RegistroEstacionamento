274d8de9f67ec82c32e44676703d5834d04b677f
DeividAlbeche <deividtrindade2005@gmail.com>
2025-04-25 12:17:04 -0300

    criando conexao com banco de dados

--- src/dao/RegistroDao.java
+++ src/dao/RegistroDao.java
@@ -1,5 +1,75 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import entidades.Registro;
+import entidades.Veiculo;
+import entidades.Cliente;
+
 public class RegistroDao {
 
+	
+	public Connection getConexao() {
+
+		// Driver
+		String driver = "com.mysql.cj.jdbc.Driver";
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// URL do banco de dados
+		String url = "jdbc:mysql://localhost:3306/registroEstacionamento";
+
+		// login
+		String login = "root";
+
+		// Senha
+		String senha = "root";
+
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, login, senha);
+			System.out.println("Conectado ao banco de dados");
+
+		} catch (SQLException e) {
+			System.err.println("Credenciais inválidas. Erro ao conectar");
+		}
+
+		return conn;
+	}
+	
+	public void inserirRegistro(Registro novaRegistro) {
+
+		String consulta = "insert into pessoa " + "(hora_entrada, hora_saida, data_registro, valor_total, id_cliente, id_veiculo) values"
+				+ " (?,?,?,?,?,?)";
+
+		try {
+			Connection conn = getConexao();
+			PreparedStatement pst = conn.prepareStatement(consulta);
+			// RETURN_GENERATED_KEYS: Retornando o id criado pelo DB
+			pst.setTimestamp(1, hora_entrada.getHora_entrada());
+			pst.setTimestamp(1, hora_saida.getHora_entrada());
+			pst.setLocaldate(3, novaPessoa.getIdade());
+			pst.setString(4, novaPessoa.getEmail());
+
+			// Executando a consulta
+			pst.executeUpdate();
+
+			pst.close();
+			conn.close();
+
+		} catch (Exception e) {
+
+		}
+
+
+	}
+	
+	
+	
 }
--- src/entidades/Registro.java
+++ src/entidades/Registro.java
@@ -54,6 +54,90 @@ public class Registro {
 				+ ", data_registro=" + data_registro + ", valor_total=" + valor_total + ", cliente=" + cliente
 				+ ", veiculo=" + veiculo + "]";
 	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public Timestamp getHora_entrada() {
+		return hora_entrada;
+	}
+
+
+
+	public void setHora_entrada(Timestamp hora_entrada) {
+		this.hora_entrada = hora_entrada;
+	}
+
+
+
+	public Timestamp getHora_saida() {
+		return hora_saida;
+	}
+
+
+
+	public void setHora_saida(Timestamp hora_saida) {
+		this.hora_saida = hora_saida;
+	}
+
+
+
+	public LocalDate getData_registro() {
+		return data_registro;
+	}
+
+
+
+	public void setData_registro(LocalDate data_registro) {
+		this.data_registro = data_registro;
+	}
+
+
+
+	public double getValor_total() {
+		return valor_total;
+	}
+
+
+
+	public void setValor_total(double valor_total) {
+		this.valor_total = valor_total;
+	}
+
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
 	
 	
 	
